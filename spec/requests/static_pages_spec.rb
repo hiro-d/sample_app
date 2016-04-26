c0845b1344e00195625ed4920f714d29ad9b6fe1
hiroki deguchi <shimamura000@gmail.com>
2016-04-27 02:35:53 +0900

    mod spec

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,7 +7,7 @@ describe "StaticPages" do
         expect(page).to have_content('Sample App')
     end
 
-    it "should have the right title" do
+    it "should have the title 'Home'" do
         visit '/static_pages/home'
         expect(page).to have_title("Ruby on Rails Tutorial Sample App")
     end
@@ -24,7 +24,7 @@ describe "StaticPages" do
         expect(page).to have_content('Help')
     end
 
-    it "should have the right title" do
+    it "should have the title 'Help'" do
         visit '/static_pages/help'
         expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
     end
@@ -36,7 +36,7 @@ describe "StaticPages" do
         expect(page).to have_content('About Us')
     end
 
-    it "should have the right title" do
+    it "should have the title 'About Us'" do
         visit '/static_pages/about'
         expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
     end
