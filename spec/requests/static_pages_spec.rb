f97a832f11ab44c13c36a7eb38cbc36de76f7c08
hiroki deguchi <shimamura000@gmail.com>
2016-04-27 02:38:53 +0900

    mod spec

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 
 describe "StaticPages" do
+
   describe "Home page" do
     it "should have the content 'Sample App'" do
         visit '/static_pages/home'
@@ -53,5 +54,4 @@ describe "StaticPages" do
         expect(page).to have_title("Ruby on Rails Tutorial Sample App | Contact")
     end
   end
-
 end
